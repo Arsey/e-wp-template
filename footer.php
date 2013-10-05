cc5dd6d9d446fe74094ab4fe9d8e8e115ec959b6
arsey <arseysensector@gmail.com>
2013-10-05 08:51:23 +0300

    implemented css sticky footer method copied from http:/ryanfait.com/sticky-footer/

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+<div class="push"></div>
+</div>
+<div class="footer"></div>
 <?php wp_footer(); ?>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="<?php td(); ?>/js/main.js"></script>
--- header.php
+++ header.php
@@ -18,9 +18,10 @@
 
 		<link rel="stylesheet" href="<?php td(); ?>/css/reset.css">
 		<link rel="stylesheet" href="<?php td(); ?>/style.css">
-		
-		<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script><![endif]-->
-		
+
+<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script><![endif]-->
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<div class="wrapper">
