bd5104a5857ccbe9836774542e4a62ee8215f0b3
Alexader <arseysensector@gmail.com>
2014-11-01 08:03:25 +0200

    refactored

--- functions.php
+++ functions.php
@@ -1,60 +1,138 @@
 <?php
-/* For adding custop settings for theme we need to use next file */
-/* ------------------------------------------------------------- */
-include('functions/theme-options.php');
-#include('functions/metabox.php');
-#include('simple-widget.php');
 
-
-/* WORK WITH I-M-A-G-E-S */
-/* For add theme support thumbnails */
-/* ------------------------------------------------------------- */
-if (function_exists('add_theme_support')) {
-	add_theme_support('post-thumbnails');
-	set_post_thumbnail_size(150, 150); // default Post Thumbnail dimensions
+function p($var, $print = true)
+{
+    if ($print === true) {
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+    } elseif ($print === false) {
+        ob_start();
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+        $out = ob_get_contents();
+        ob_clean();
+        return $out;
+    }
 }
 
+/**
+ * Registering custom post types
+ */
+add_action('init', 'create_post_type');
+function create_post_type()
+{
+    /**
+     * Slides
+     */
+    register_post_type(
+        'theme_slides',
+        array(
+            'labels' => array(
+                'name' => __('Slides'),
+                'singular_name' => __('Slide')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'exclude_from_search' => true,
+            'supports' => array(
+                'thumbnail',
+                'title'
+            )
+        )
+    );
+}
 
-/* If we need some custom cropped images after downloading to server we need to use */
-/* ------------------------------------------------------------- */
+/**
+ * For adding the custom settings for theme we need to use next file
+ */
+include('functions/theme-options.php');
+include('functions/metabox.php');
+
+/**
+ * Make the theme supports thumbnails
+ */
+if (function_exists('add_theme_support')) {
+    add_theme_support('post-thumbnails');
+    set_post_thumbnail_size(150, 150); // default Post Thumbnail dimensions
+}
+
+/**
+ * Custom image sizes
+ */
 #if (function_exists('add_image_size')) {
 #   add_image_size('some-size', $width, $height);
 #}
 
-/* this function display image of any post if it has thumbnail or attachments */
-/* ------------------------------------------------------------- */
-
-function get_image_any_case($id, $size) {
-	if (function_exists("has_post_thumbnail") && has_post_thumbnail()) {
-		echo get_the_post_thumbnail($id, $size);
-	} else {
-		$args = array(
-			'post_type' => 'attachment',
-			'numberposts' => 1,
-			'post_mime_type' => 'image',
-			'post_parent' => $id,
-			'sort_order' => 'menu_order'
-		);
-		$attachments = get_posts($args);
-		if ($attachments) {
-			$title = htmlspecialchars($attachments[0]->post_title);
-			echo '<img src="', wp_get_attachment_thumb_url($attachments[0]->ID), '" class=""  alt="', $title, '" title="', $title, '" />';
-		}
-	}
+/**
+ * This function displays an image of the post if it has the thumbnail or the attachments
+ * @param $id
+ * @param $size
+ */
+function get_image_any_case($id, $size)
+{
+    if (function_exists("has_post_thumbnail") && has_post_thumbnail()) {
+        echo get_the_post_thumbnail($id, $size);
+    } else {
+        $args = array(
+            'post_type' => 'attachment',
+            'numberposts' => 1,
+            'post_mime_type' => 'image',
+            'post_parent' => $id,
+            'sort_order' => 'menu_order'
+        );
+        $attachments = get_posts($args);
+        if ($attachments) {
+            $title = htmlspecialchars($attachments[0]->post_title);
+            echo '<img src="', wp_get_attachment_thumb_url($attachments[0]->ID), '" class=""  alt="', $title, '" title="', $title, '" />';
+        }
+    }
 }
 
-/* WORK WITH M-E-N-U-S */
-/* For using dynamic menus */
-/* ------------------------------------------------------------- */
-#add_action('init', 'register_custom_menus');
-#function register_custom_menus() {
-#    register_nav_menu('main_menu', __('Main Menu'));
-#    register_nav_menu('footer_menu', __('Footer Menu'));
-#}
+/**
+ * Allow SVG through WordPress Media Uploader
+ * @param $mimes
+ * @return mixed
+ */
+function cc_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+
+add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+ * Dynamic menus
+ */
+add_action('init', 'register_custom_menus');
+function register_custom_menus()
+{
+    register_nav_menu('main_menu', __('Main Menu'));
+    register_nav_menu('services-menu', __('Services Menu'));
+}
 
 
+function build_tree(array &$elements, $parentId = 0)
+{
+    $branch = array();
+    foreach ($elements as $element) {
+        if ($element->menu_item_parent == $parentId) {
+            $children = build_tree($elements, $element->ID);
+            if ($children) {
+                $element->children = $children;
+            }
+            $branch[$element->ID] = $element;
+            unset($elements[$element->ID]);
+        }
+    }
+    return $branch;
+}
 
-/* WORK WITH S-I-D-E-B-A-R-S */
+/**
+ * Dynamic sidebars
+ */
 #if (function_exists('register_sidebar')) {
 #    register_sidebar(array(
 #        'name' => 'One',
@@ -66,11 +144,107 @@ function get_image_any_case($id, $size) {
 #    ));
 #}
 
+/**
+ * This function cuts any text to max length
+ * @param $string
+ * @param $max_length
+ * @return string
+ */
+function cut_text($string, $max_length)
+{
+    if (strlen($string) > $max_length) {
+        $string = mb_substr($string, 0, $max_length);
+        $pos = strrpos($string, " ");
+        if ($pos === false) {
+            return mb_substr($string, 0, $max_length) . "...";
+        }
+        return mb_substr($string, 0, $pos) . "...";
+    } else {
+        return $string;
+    }
+}
 
+/**
+ * Pagination function
+ * @param $title
+ * @param $mux_num_pages
+ * @param string $type
+ */
+function pagination($title = 'Pages: ', $mux_num_pages = 3, $type = 'plain')
+{
+    if ($mux_num_pages > 1) {
+        echo '<div class="pagination">' . $title;
 
-/* WORK WITH C-O-M-M-E-N-T-S */
+        global $wp_query;
+        $big = 999999999; // need an unlikely integer
+        $pagination = paginate_links(array(
+            'base' => str_replace($big, '%#%', get_pagenum_link($big)),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $mux_num_pages,
+            'prev_next' => false,
+            'type' => $type,
+        ));
+
+        if ($type == 'plain') {
+            echo $pagination;
+        } else if ($type = 'array') {
+            foreach ($pagination as $link) {
+                echo preg_replace('~<a~', "$0 rel=nofollow ", $link);
+            }
+        }
+        ?>
+        </div>
+    <?php
+    }
+}
+
+/**
+ * Returns the number of category posts
+ * @param string $input
+ * @return mixed
+ */
+function wt_get_category_count($input = '')
+{
+    global $wpdb;
+    if ($input == '') {
+        $category = get_the_category();
+        return $category[0]->category_count;
+    } elseif (is_numeric($input)) {
+        $SQL = "SELECT $wpdb->term_taxonomy.count FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id=$wpdb->term_taxonomy.term_id AND $wpdb->term_taxonomy.term_id=$input";
+        return $wpdb->get_var($SQL);
+    } else {
+        $SQL = "SELECT $wpdb->term_taxonomy.count FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id=$wpdb->term_taxonomy.term_id AND $wpdb->terms.slug='$input'";
+        return $wpdb->get_var($SQL);
+    }
+}
+
+/**
+ * Returns category slug by category id
+ * @param $cat_id
+ * @return mixed
+ */
+function get_cat_slug($cat_id)
+{
+    $cat_id = (int)$cat_id;
+    $category = & get_category($cat_id);
+    return $category->slug;
+}
+
+/**
+ * Shortcut function for bloginfo('template_directory')
+ * @param bool $print_out
+ * @return string|void
+ */
+function td($print_out = true)
+{
+    if ($print_out)
+        bloginfo('template_directory');
+    else
+        return get_bloginfo('template_directory');
+}
+
 if (!function_exists('twentyten_comment')) :
-
 	function twentyten_comment($comment, $args, $depth) {
 		$GLOBALS['comment'] = $comment;
 		switch ($comment->comment_type) :
@@ -113,89 +287,4 @@ if (!function_exists('twentyten_comment')) :
 					break;
 			endswitch;
 		}
-
 	endif;
-
-
-	/* O-T-H-E-R      F-U-N-C-T-I-O-N-S */
-
-	/* this function cut any text to max length */
-
-	function cut_text($string, $max_length) {
-		if (strlen($string) > $max_length) {
-			$string = mb_substr($string, 0, $max_length);
-			$pos = strrpos($string, " ");
-			if ($pos === false) {
-				return mb_substr($string, 0, $max_length) . "...";
-			}
-			return mb_substr($string, 0, $pos) . "...";
-		} else {
-			return $string;
-		}
-	}
-
-	/* function for pagination links on posts in loop */
-
-	function pagination($title, $mux_num_pages, $type = 'plain') {
-		if ($mux_num_pages > 1) {
-			echo '<div class="pagination">' . $title;
-
-			global $wp_query;
-			$big = 999999999; // need an unlikely integer
-			$pagination = paginate_links(array(
-				'base' => str_replace($big, '%#%', get_pagenum_link($big)),
-				'format' => '?paged=%#%',
-				'current' => max(1, get_query_var('paged')),
-				'total' => $mux_num_pages,
-				'prev_next' => false,
-				'type' => $type,
-			));
-
-			if ($type == 'plain') {
-				echo $pagination;
-			} else if ($type = 'array') {
-				foreach ($pagination as $link) {
-					echo preg_replace('~<a~', "$0 rel=nofollow ", $link);
-				}
-			}
-			?>
-		</div>
-		<?php
-	}
-}
-
-/* this function return count of posts in category */
-
-function wt_get_category_count($input = '') {
-	global $wpdb;
-	if ($input == '') {
-		$category = get_the_category();
-		return $category[0]->category_count;
-	} elseif (is_numeric($input)) {
-		$SQL = "SELECT $wpdb->term_taxonomy.count FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id=$wpdb->term_taxonomy.term_id AND $wpdb->term_taxonomy.term_id=$input";
-		return $wpdb->get_var($SQL);
-	} else {
-		$SQL = "SELECT $wpdb->term_taxonomy.count FROM $wpdb->terms, $wpdb->term_taxonomy WHERE $wpdb->terms.term_id=$wpdb->term_taxonomy.term_id AND $wpdb->terms.slug='$input'";
-		return $wpdb->get_var($SQL);
-	}
-}
-
-/* this function returning a slag of any posts category */
-
-function get_cat_slug($cat_id) {
-	$cat_id = (int) $cat_id;
-	$category = &get_category($cat_id);
-	return $category->slug;
-}
-
-/**
- * Returns template directory
- */
-function td($print_out = true) {
-	if ($print_out) {
-		bloginfo('template_directory');
-		return;
-	}
-	return get_bloginfo('template_directory');
-}
-
